6de9100d8b85b467dcda6a4b7b337886c7ea4c05
Josh Marshall <josh@jmarshall.com.au>
2020-09-13 13:05:17 +1000

    Add files

---
+++ tests/testdb2modelconf.php
@@ -0,0 +1,12 @@
+<?php
+$db_host                    = 'database';
+$db_host                    = 'database';
+$db_name                    = 'testdb';
+$db_username                = 'root';
+$db_password                = 'root';
+$models_output_dir          = __DIR__ . '/Auto';
+$model_to_extend            = '\MyAppTest\ORMBaseClass';
+$controller_model_to_extend = '\MyAppTest\Controller';
+$use_namespaces             = false;
+$default_namespace          = 'MyAppTest';
+$plural_tables              = [];
